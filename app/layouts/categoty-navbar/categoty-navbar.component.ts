1a9b5652f34c9b0d055ee1d4e89105751d3b7fea
saba jokhadze <saba.joxadze.04@gmail.com>
2023-05-18 11:05:21 +0400

    add files

---
+++ app/layouts/categoty-navbar/categoty-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'categoty-navbar',
+  templateUrl: './categoty-navbar.component.html',
+  styleUrls: ['./categoty-navbar.component.css']
+})
+export class CategotyNavbarComponent {
+
+}
